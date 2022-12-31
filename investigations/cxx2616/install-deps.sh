8b0ac1644524a812f56f97efabf3f0f5702cc8b2
Kevin Albertson <kevin.eric.albertson@gmail.com>
2022-12-31 14:28:40 -0500

    account for csfle-markup build failure

--- investigations/cxx2616/install-deps.sh
+++ investigations/cxx2616/install-deps.sh
@@ -1,9 +1,16 @@
-export LIBMONGOCRYPT_EXTRA_CMAKE_OPTIONS='-G "Visual Studio 17 2022" -A x64'
+export LIBMONGOCRYPT_EXTRA_CMAKE_OPTIONS='-A x64'
 export LIBMONGOCRYPT_GITREF="1.6.0"
 export LIBMONGOCRYPT_INSTALL_PREFIX="$(pwd)/install/libmongocrypt-1.6.0"
 ./etc/install_libmongocrypt.sh
+# Install will fail with VS 2022 due to building csfle-markup.
+# Apply a patch to remove csfle-markup, and try to install again.
+root=$(pwd)
+cd ./install/libmongocrypt-1.6.0/libmongocrypt
+git apply $root/etc/remove-csfle-markup.patch
+. ./etc/find_cmake.sh
+$CMAKE --build . --target install -j8
 
 
-export MONGO_C_DRIVER_EXTRA_CMAKE_OPTIONS='-G "Visual Studio 17 2022" -A x64'
+export MONGO_C_DRIVER_EXTRA_CMAKE_OPTIONS='-A x64'
 export MONGO_C_DRIVER_GITREF="1.23.1"
 ./etc/install_mongo_c_driver.sh
