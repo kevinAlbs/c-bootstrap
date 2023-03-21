02552c01fc719f3854b1af5cc34469e28adb6e14
Kevin Albertson <kevin.albertson@mongodb.com>
2023-03-21 08:57:19 -0400

    fix path for kmip_dump

--- tools/kmip_dump/configure.sh
+++ tools/kmip_dump/configure.sh
@@ -1,7 +1,7 @@
 cmake \
-    -DCMAKE_PREFIX_PATH=/Users/kevin.albertson/install/mongo-c-driver-master \
+    -DCMAKE_PREFIX_PATH=/Users/kevin.albertson/code/c-bootstrap/install/mongo-c-driver-1.23.2 \
     -DCMAKE_BUILD_TYPE="Debug" \
-    -DCMAKE_C_COMPILER="/Users/kevin.albertson/bin/llvm-11.0.0/bin/clang" \
+    -DCMAKE_C_COMPILER="clang" \
     -DCMAKE_C_COMPILER_LAUNCHER="ccache" \
     -DCMAKE_C_FLAGS="-fsanitize=address" \
     -S/Users/kevin.albertson/code/c-bootstrap/tools/kmip_dump \
