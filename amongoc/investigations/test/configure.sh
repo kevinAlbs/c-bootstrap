bdba4e8cc210014f805be36439725b4814f6e86a
Kevin Albertson <kevin.albertson@mongodb.com>
2025-01-09 22:45:26 +0000

    fix linker error

--- amongoc/investigations/test/configure.sh
+++ amongoc/investigations/test/configure.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
 VCPKG_ROOT="$HOME/vcpkg"
-AMONGOC_INSTALL="/home/admin/c-bootstrap/amongoc/install/mongo-c-driver-async-pr2"
+AMONGOC_INSTALL="/home/admin/mongo-c-driver-async/mongo-c-driver-async-0.1.0-async-pr4"
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
       -DCMAKE_PREFIX_PATH=$AMONGOC_INSTALL \
--- amongoc/investigations/test/hello-amongoc.c
+++ amongoc/investigations/test/hello-amongoc.c
@@ -1,49 +1,173 @@
-#include <amongoc/amongoc.h> // Make all APIs visible
+#include <amongoc/amongoc.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-// end:headers
+#include "bson/view.h"
+#include <bson/iterator.h>
+#include <bson/mut.h>
 
-amongoc_box on_connect(amongoc_box userdata, amongoc_status *status, amongoc_box result);
+#include "mlib/str.h"
 
-int main(void)
-{
-   amongoc_loop loop;
-   amongoc_default_loop_init(&loop);
+/**
+ * @brief Shared state for the application. This is passed through the app as pointer stored
+ * in a box
+ */
+typedef struct app_state {
+    // The connection to a server
+    amongoc_client* client;
+} app_state;
 
-   // Initiate a connection
-   amongoc_emitter em = amongoc_client_new(&loop, "mongodb://localhost:27017");
-   // Set the continuation
-   em = amongoc_then(em, &on_connect);
-   // Run the program
-   amongoc_detach_start(em);
-   amongoc_default_loop_run(&loop);
-   // Clean up
-   amongoc_default_loop_destroy(&loop);
-   return 0;
+/**
+ * @brief Write the content of a BSON document in JSON-like format to the given output
+ */
+static void print_bson(FILE* into, bson_view doc, mlib_str_view indent);
+
+/** after_hello()
+ * @brief Handle the `hello` response from the server
+ *
+ * @param state_ptr Pointer to the `app_state`
+ * @param resp_data A `bson_mut` object that contains the response message
+ * @return amongoc_box Returns `amongoc_nil`
+ */
+amongoc_box after_hello(amongoc_box state_ptr, amongoc_status*, amongoc_box resp_data) {
+    (void)state_ptr;
+    bson_view resp = bson_view_from(amongoc_box_cast(bson_doc, resp_data));
+    // Just print the response message
+    fprintf(stdout, "Got response: ");
+    print_bson(stdout, resp, mlib_str_view_from(""));
+    fputs("\n", stdout);
+    amongoc_box_destroy(resp_data);
+    return amongoc_nil;
 }
+// end.
 
-// on_connect def
-amongoc_box on_connect(amongoc_box userdata, amongoc_status *status, amongoc_box result)
-{
-   // We don't use the userdata
-   (void)userdata;
-   // Check for an error
-   if (amongoc_is_error(*status))
-   {
-      char *msg = amongoc_status_strdup_message(*status);
-      fprintf(stderr, "Error while connecting to server: %s\n", msg);
-      free(msg);
-   }
-   else
-   {
-      printf("Successfully connected!\n");
-      amongoc_client *client;
-      amongoc_box_take(client, result);
-      // `cl` now stores a valid client. We don't do anything else, so just delete it:
-      amongoc_client_delete(client);
-   }
-   amongoc_box_destroy(result);
-   return amongoc_nil;
+/** after_connect_say_hello()
+ * @brief Handle the connection to a server. Sends a "hello" message
+ *
+ * @param state_ptr Pointer to the `app_state`
+ * @param cl_box An `amongoc_client*`
+ * @return amongoc_emitter
+ */
+amongoc_emitter after_connect_say_hello(amongoc_box state_ptr, amongoc_status, amongoc_box cl_box) {
+    printf("Connected to server\n");
+    // Store the connection in our app state
+    amongoc_box_take(amongoc_box_cast(app_state*, state_ptr)->client, cl_box);
+
+    // Create a "hello" command
+    bson_doc doc = bson_new();
+    bson_mut mut = bson_mutate(&doc);
+    bson_insert(&mut, "hello", "1");
+    bson_insert(&mut, "$db", "test");
+    amongoc_emitter em = amongoc_client_command(amongoc_box_cast(app_state*, state_ptr)->client,
+                                                bson_view_from(mut));
+    bson_delete(doc);
+
+    em = amongoc_then(em,
+                      amongoc_async_forward_errors,
+                      mlib_default_allocator,
+                      state_ptr,
+                      after_hello);
+    return em;
 }
-// on_connect end
+// end.
+
+int main(int argc, char const* const* argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <uri>\n", argv[0]);
+        return 1;
+    }
+    const char* const uri = argv[1];
+
+    amongoc_loop loop;
+    amongoc_default_loop_init(&loop);
+
+    struct app_state state = {0};
+
+    amongoc_emitter em = amongoc_client_new(&loop, uri);
+    struct timespec dur;
+    dur.tv_sec  = 5;
+    dur.tv_nsec = 0;
+    em          = amongoc_timeout(&loop, em, dur);
+
+    em = amongoc_let(em,
+                     amongoc_async_forward_errors,
+                     mlib_default_allocator,
+                     amongoc_box_pointer(&state),
+                     after_connect_say_hello);
+
+    amongoc_status    fin_status = amongoc_okay;
+    amongoc_operation op         = amongoc_tie(em, &fin_status, NULL, mlib_default_allocator);
+    amongoc_start(&op);
+    amongoc_default_loop_run(&loop);
+    amongoc_operation_delete(op);
+
+    // Destroy the connection since we are done with it (this is a no-op for a null connection)
+    amongoc_client_delete(state.client);
+    amongoc_default_loop_destroy(&loop);
+
+    if (amongoc_is_error(fin_status)) {
+        char* m = amongoc_status_strdup_message(fin_status);
+        fprintf(stderr, "An error occurred: %s\n", m);
+        free(m);
+        return 2;
+    } else {
+        printf("Okay\n");
+        return 0;
+    }
+}
+// end.
+
+static void print_bson(FILE* into, bson_view doc, mlib_str_view indent) {
+    fprintf(into, "{\n");
+    bson_foreach(it, doc) {
+        mlib_str_view str = bson_key(it);
+        fprintf(into, "%*s  \"%s\": ", (int)indent.len, indent.data, str.data);
+        bson_value_ref val = bson_iterator_value(it);
+        switch (val.type) {
+        case bson_type_eod:
+        case bson_type_double:
+            fprintf(into, "%f,\n", val.double_);
+            break;
+        case bson_type_utf8:
+            fprintf(into, "\"%s\",\n", val.utf8.data);
+            break;
+        case bson_type_document:
+        case bson_type_array: {
+            mlib_str  i2     = mlib_str_append(indent, "  ");
+            bson_view subdoc = bson_iterator_value(it).document;
+            print_bson(into, subdoc, mlib_str_view_from(i2));
+            mlib_str_delete(i2);
+            fprintf(into, ",\n");
+            break;
+        }
+        case bson_type_undefined:
+            fprintf(into, "[undefined],\n");
+            break;
+        case bson_type_bool:
+            fprintf(into, val.bool_ ? "true,\n" : "false,\n");
+            break;
+        case bson_type_null:
+            fprintf(into, "null,\n");
+            break;
+        case bson_type_int32:
+            fprintf(into, "%d,\n", val.int32);
+            break;
+        case bson_type_int64:
+            fprintf(into, "%ld,\n", val.int64);
+            break;
+        case bson_type_timestamp:
+        case bson_type_decimal128:
+        case bson_type_maxkey:
+        case bson_type_minkey:
+        case bson_type_oid:
+        case bson_type_binary:
+        case bson_type_datetime:
+        case bson_type_regex:
+        case bson_type_dbpointer:
+        case bson_type_code:
+        case bson_type_symbol:
+        case bson_type_codewscope:
+            fprintf(into, "[[printing unimplemented for this type]],\n");
+            break;
+        }
+    }
+    fprintf(into, "%*s}", (int)indent.len, indent.data);
+}
