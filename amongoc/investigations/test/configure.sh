6b0ca1b74c4dcb15d5f79efa6d54ae1794ad0d95
Kevin Albertson <kevin.albertson@mongodb.com>
2025-01-22 08:04:36 -0500

    update amongoc test for macOS

--- amongoc/investigations/test/configure.sh
+++ amongoc/investigations/test/configure.sh
@@ -2,8 +2,14 @@
 
 SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
 VCPKG_ROOT="$HOME/vcpkg"
-AMONGOC_INSTALL="/home/admin/mongo-c-driver-async/mongo-c-driver-async-0.1.0-async-pr4"
+AMONGOC_INSTALL="$HOME/amongoc-0.1.0"
+
+# For macOS, specify a newer clang:
+# Use clang 19.
+export CC=/opt/homebrew/opt/llvm/bin/clang
+export CXX=/opt/homebrew/opt/llvm/bin/clang++
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
       -DCMAKE_PREFIX_PATH=$AMONGOC_INSTALL \
+      -DCMAKE_C_STANDARD=23 \
       -Bcmake-build 
