097c75ed9f2756d56cc34bf3f0dfde90899ca9a0
Kevin Albertson <kevin.albertson@mongodb.com>
2025-01-25 08:49:52 -0500

    fix benchmark

--- investigations/HELP-69502/benchmark-sd-copy/benchmark-sd-copy.c
+++ investigations/HELP-69502/benchmark-sd-copy/benchmark-sd-copy.c
@@ -7,8 +7,6 @@
 
 UBENCH_EX(mongoc_server_description_new_copy, sdcopy)
 {
-    mongoc_init();
-
     mongoc_client_t *client = mongoc_client_new("mongodb://localhost:27017");
     bson_error_t error;
     mongoc_server_description_t *sd = mongoc_client_select_server(client, false, NULL, &error);
@@ -18,13 +16,20 @@ UBENCH_EX(mongoc_server_description_new_copy, sdcopy)
         for (size_t i = 0; i < 120; i++)
         {
             mongoc_server_description_t *sd_copy = mongoc_server_description_new_copy(sd);
+            UBENCH_DO_NOTHING(sd_copy);
             mongoc_server_description_destroy(sd_copy);
         }
     }
 
     mongoc_server_description_destroy(sd);
     mongoc_client_destroy(client);
-    mongoc_cleanup();
 }
 
-UBENCH_MAIN();
+UBENCH_STATE();
+
+int main(int argc, const char *const argv[])
+{
+    mongoc_init();
+    ubench_main(argc, argv);
+    mongoc_cleanup();
+}
